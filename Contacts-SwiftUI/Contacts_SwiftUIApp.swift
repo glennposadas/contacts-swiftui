9b5073ed3f5fcfaed052e6e56c089bd0e6c05b54
Glenn Posadas <hello@glennvon.com>
2020-10-10 22:17:37 +0800

    add base proj.

---
+++ Contacts-SwiftUI/Contacts_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  Contacts_SwiftUIApp.swift
+//  Contacts-SwiftUI
+//
+//  Created by Glenn Von Posadas on 10/10/20.
+//
+
+import SwiftUI
+
+@main
+struct Contacts_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
